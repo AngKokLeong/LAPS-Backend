8708f64b938c7fc43730f88eaf3012b7bd83c294
chernpo <266276775+chernpo@users.noreply.github.com>
2026-04-05 22:20:19 +0800

    (test) Leave Entitlement

---
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/LeaveEntitlement.java
@@ -0,0 +1,21 @@
+package iss.nus.edu.sg.leave_application_processing_system.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "Leave Entitlement")
+public class LeaveEntitlement {
+
+	@Id
+	private int entitlementId;
+	@Enumerated (EnumType.STRING)
+	private LeaveType leavetype;
+	private int year;
+	private int totalDays;
+	private int usedDays;
+	
+}
