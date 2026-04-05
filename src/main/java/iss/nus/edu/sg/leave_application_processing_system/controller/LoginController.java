53f3845ee22e9d9fff513a89751a3f1b5b9defd0
chernpo <266276775+chernpo@users.noreply.github.com>
2026-04-05 11:48:03 +0800

    update success html

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/LoginController.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/LoginController.java
@@ -11,22 +11,43 @@ public class LoginController {
 
 	@GetMapping ("/")
 	public String showLandingPage() {
-		return "login";       // returns login.html
+		return "login";       
 	}
 	
 	@GetMapping ("/login")
 	public String loginPage() {	
-		return("login");
+		return "login";
 	}
 	
 	@PostMapping ("/validate") 
 	public String validateLogin (@RequestParam String username, @RequestParam String password, Model model) {
 		if (username.equalsIgnoreCase("john@company.com") && password.equalsIgnoreCase("any")) {
+			model.addAttribute("username", username);
 			return "success";
 		}
 		else {
 			return "failure";
 			}
+	}
+	
+	@GetMapping ("/success/dashboard")
+	public String viewDashboard() {	
+		return("dashboard");
+	}
+	
+	@GetMapping ("/success/applyleave")
+	public String applyLeavePage() {	
+		return("applyleave");
+	}
+	
+	@PostMapping("/success/applyleave/apply")
+	public String leaveApplication() {
+		return("myleaves");
+	}
+	
+	@GetMapping ("/success/myleaves")
+	public String viewLeavePage() {	
+		return("myleaves");
 	}
 	
 }
