c4d5a704e92df73de4de4c28297ad0266790d1a6
loh-chunheng <chunheng.loh@gmail.com>
2026-04-05 22:15:36 +0800

    included constructors, getters, setters for LeaveApplication Entity, added LeaveStatus Enum in helper foler

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/LeaveApplication.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/LeaveApplication.java
@@ -1,7 +1,9 @@
 package iss.nus.edu.sg.leave_application_processing_system.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,8 +18,81 @@ public class LeaveApplication {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	private Employee employee;
+	private Enum<LeaveType> leaveType;
+	
 	private LocalDate startDate;
 	private LocalDate endDate;
+	private LocalDateTime appliedAt;
+	private Enum<LeaveStatus> status;
 	
+	public LeaveApplication() {}
+
+	public LeaveApplication(Employee employee, LeaveType leaveType, LocalDate startDate, LocalDate endDate) {
+		this.employee = employee;
+		this.leaveType = leaveType;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.appliedAt = LocalDateTime.now();
+		this.status = LeaveStatus.PENDING;
+	}
+	
+	//getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public Enum<LeaveType> getLeaveType() {
+		return leaveType;
+	}
+
+	public void setLeaveType(Enum<LeaveType> leaveType) {
+		this.leaveType = leaveType;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public LocalDateTime getAppliedAt() {
+		return appliedAt;
+	}
+
+	public void setAppliedAt(LocalDateTime appliedAt) {
+		this.appliedAt = appliedAt;
+	}
+
+	public Enum<LeaveStatus> getStatus() {
+		return status;
+	}
+
+	public void setStatus(Enum<LeaveStatus> status) {
+		this.status = status;
+	}
+		
 	
 }
