ac16b1cd19bd4851f26f7b7b7aaea22679cb25c4
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-12 09:53:12 +0000

    LAPSB-46 modified the landingPage() in LandingController to test Annaul Leave application validation

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/LandingController.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/LandingController.java
@@ -1,8 +1,18 @@
 package iss.nus.edu.sg.leave_application_processing_system.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import iss.nus.edu.sg.leave_application_processing_system.service.LeaveApplicationService;
+import iss.nus.edu.sg.leave_application_processing_system.service.DTO.AnnualLeaveServiceDTO;
+import iss.nus.edu.sg.leave_application_processing_system.service.DTO.ServiceDTO;
+
 
 
 
@@ -10,10 +20,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/landing")
 public class LandingController {
-    
+
+    private LeaveApplicationService leaveApplicationService;
+
+    public LandingController(@Qualifier("AnnualLeaveApplicationService") LeaveApplicationService leaveApplicationService){
+        this.leaveApplicationService = leaveApplicationService;
+    }
+
 
     @GetMapping("/")
-    public String landingPage() {
+    public String landingPage(Model model) {
+
+        ServiceDTO annualLeaveApplicationDTO = new AnnualLeaveServiceDTO(
+                                            LocalDateTime.of(2026, 05, 04, 11, 0),
+                                            LocalDateTime.of(2026, 05, 11, 11, 0)
+                                        );
+        
+        boolean status = leaveApplicationService.submitApplication(annualLeaveApplicationDTO);
+
+        model.addAttribute("test", status);
+
+
         return "landing";
     }
     
