bd5a2e6814193f2c733b595c6f68d8daa65bc14e
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-12 04:53:15 +0000

    LAPSB-39 added Specification pattern into Service layer

---
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/service/business_logic/specification/composite/OrSpecification.java
@@ -0,0 +1,23 @@
+package iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification.composite;
+
+import java.util.function.BiPredicate;
+
+import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification.Specification;
+
+public class OrSpecification<T> extends CompositeSpecification<T> {
+
+    private final Specification<T> firstSpecification;
+    private final Specification<T> secondSpecification;
+
+    public OrSpecification(Specification<T> firstSpecification, Specification<T> secondSpecification){
+        this.firstSpecification = firstSpecification;
+        this.secondSpecification = secondSpecification;
+    }
+
+    @Override
+    public BiPredicate<T, T> toPredicate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toPredicate'");
+    }
+    
+}
