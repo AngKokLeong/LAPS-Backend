ee106ceb374f7afcb6c9c1c6d8407c2c5cd6e553
loh-chunheng <chunheng.loh@gmail.com>
2026-04-12 10:40:44 +0800

    refactor(manager): consolidate base URL mapping to /manager

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/ManagerController.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/ManagerController.java
@@ -3,10 +3,12 @@ package iss.nus.edu.sg.leave_application_processing_system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/manager")
 public class ManagerController {
 	
 	@GetMapping("/team-leave-history")
