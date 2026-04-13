33d12cddaa4b9716192b14aad1f7f78f9f6af515
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-04-13 04:31:29 +0000

    LAPSB-26 fixed the LeaveEntitlement entity class and modified the attribute from year to yearApplied

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/LeaveEntitlement.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/LeaveEntitlement.java
@@ -1,21 +1,33 @@
 package iss.nus.edu.sg.leave_application_processing_system.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table (name = "Leave Entitlement")
 public class LeaveEntitlement {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long entitlementId;
+
+	@ManyToOne
+	@JoinColumn(name = "employee_id")
 	private Employee employeeId;
-	@Enumerated (EnumType.STRING)
-	private LeaveType leavetype;
-	private int year;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private LeaveType leaveType = LeaveType.ANNUAL;
+	
+	private int yearApplied;
 	private int totalDays;
 	private int usedDays;
 	
