9fcb2b4078e0829e85242df3d30683cd5c59ee74
loh-chunheng <chunheng.loh@gmail.com>
2026-04-13 20:09:05 +0800

    admin's page redirect to dashboard page if not session.userRole is not admin

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/AdminController.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/AdminController.java
@@ -4,33 +4,75 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-  @GetMapping("/employee-management")
-	public String employeeManagement() {     
+	@GetMapping("/employee-management")
+	public String employeeManagement(HttpSession session) {  
+	  	String role = (String) session.getAttribute("userRole");
+		
+		if (role == null) return "redirect:/login";
+
+	    if (!"admin".equals(role)) {
+	        return "redirect:/staff/dashboard"; // Send them home if they aren't a admin
+	    }
+	    
 		return "employee-management";       
 	}
 	
 	@GetMapping("/leave-type-management")
-	public String leaveTypeManagement() {
+	public String leaveTypeManagement(HttpSession session) {
+	  	String role = (String) session.getAttribute("userRole");
+		
+		if (role == null) return "redirect:/login";
+
+	    if (!"admin".equals(role)) {
+	        return "redirect:/staff/dashboard"; // Send them home if they aren't a admin
+	    }
+	    
 		return "leave-type-management";       
 	}
 	
 	@GetMapping("/leave-entitlement-management")
-	public String leaveEntitlementManagement() {
+	public String leaveEntitlementManagement(HttpSession session) {
+	  	String role = (String) session.getAttribute("userRole");
+		
+		if (role == null) return "redirect:/login";
+
+	    if (!"admin".equals(role)) {
+	        return "redirect:/staff/dashboard"; // Send them home if they aren't a admin
+	    }
+	    
 		return "leave-entitlement-management";       
 	}
 
-  @GetMapping("/email-template-management")
-	public String emailTemplateManagement() {
-		return "email-template-management";       
+	@GetMapping("/email-template-management")
+	public String emailTemplateManagement(HttpSession session) {
+	  	String role = (String) session.getAttribute("userRole");
+		
+		if (role == null) return "redirect:/login";
+
+	    if (!"admin".equals(role)) {
+	        return "redirect:/staff/dashboard"; // Send them home if they aren't a admin
+	    }
+	    
+	    return "email-template-management";       
 	}
 
-  @GetMapping("/email-template")
-  public String emailTemplate() {
-		return "email-template";       
+	@GetMapping("/email-template")
+	public String emailTemplate(HttpSession session) {
+	  	String role = (String) session.getAttribute("userRole");
+		
+		if (role == null) return "redirect:/login";
+
+	    if (!"admin".equals(role)) {
+	        return "redirect:/staff/dashboard"; // Send them home if they aren't a admin
+	    }
+	    
+	    return "email-template";       
 	}
 
 }
