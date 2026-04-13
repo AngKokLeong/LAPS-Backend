a6d62975362290be8e1a2d95938fbf1402e101d3
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-04-13 04:30:27 +0000

    LAPSB-26 modified the Employee entity class by adding accessor methods and constructors

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/Employee.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/Employee.java
@@ -1,13 +1,54 @@
 package iss.nus.edu.sg.leave_application_processing_system.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 @Entity
-@Table(name="Employee")
+
 public class Employee {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String fname;
+  
+  
   private String lname;
+
+  public Employee(){}
+
+  public Employee(String fname, String lname){
+    this.fname = fname;
+    this.lname = lname;
+  }
+
+
+  public Long getId(){
+    return this.id;
+  }
+
+
+  public void setFName(String fname){
+    this.fname = fname;
+  }
+
+  public String getFName(){
+    return this.fname;
+  }
+
+  public void setLName(String lname){
+    this.lname = lname;
+  }
+
+  public String getLName(){
+    return this.lname;
+  }
+
+
 }
