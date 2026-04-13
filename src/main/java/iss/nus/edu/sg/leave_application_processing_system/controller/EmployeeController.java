48238237270df5f0436f7bc12e72ebb359c0b9cf
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-04-13 04:32:49 +0000

    LAPSB-26 added EmployeeController

---
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package iss.nus.edu.sg.leave_application_processing_system.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import iss.nus.edu.sg.leave_application_processing_system.model.Employee;
+import iss.nus.edu.sg.leave_application_processing_system.repo.EmployeeRepository;
+
+@Controller
+public class EmployeeController {
+
+	
+	
+	@GetMapping ("/employees")
+	public String viewEmployees(Model model) {
+		List<Employee> fakeEmployees = List.of(
+				 new Employee("John", "Doe"),
+				 new Employee("Sarah", "Tan"),
+				 new Employee("Michael", "Lee")
+				 );
+
+
+		
+		 model.addAttribute("employees", "fakeEmployees");
+		 return "employee-management";
+	     // model.addAttribute("employees", repo.findAll());
+	     // return "employee-management";
+	}
+	
+}
+	
+	
