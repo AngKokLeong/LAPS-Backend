afc6e97fbe681797a9b396e71871dc94e08bba38
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-14 16:38:51 +0800

    LAPSB-61 removed lombok annotation and added constructors and accessor methods

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/DTO/AnnualLeaveControllerDTO.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/DTO/AnnualLeaveControllerDTO.java
@@ -2,13 +2,7 @@ package iss.nus.edu.sg.leave_application_processing_system.controller.DTO;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AnnualLeaveControllerDTO implements ControllerDTO{
     
     private LocalDateTime leavePeriodStart;
@@ -17,6 +11,30 @@ public class AnnualLeaveControllerDTO implements ControllerDTO{
     @Override
     public ControllerDTO getAllAttribute() {
         return this;
+    }
+
+    public AnnualLeaveControllerDTO(LocalDateTime leavePeriodStart, LocalDateTime leavePeriodEnd) {
+        this.leavePeriodStart = leavePeriodStart;
+        this.leavePeriodEnd = leavePeriodEnd;
+    }
+
+    public AnnualLeaveControllerDTO() {
+    }
+
+    public LocalDateTime getLeavePeriodStart() {
+        return leavePeriodStart;
+    }
+
+    public void setLeavePeriodStart(LocalDateTime leavePeriodStart) {
+        this.leavePeriodStart = leavePeriodStart;
+    }
+
+    public LocalDateTime getLeavePeriodEnd() {
+        return leavePeriodEnd;
+    }
+
+    public void setLeavePeriodEnd(LocalDateTime leavePeriodEnd) {
+        this.leavePeriodEnd = leavePeriodEnd;
     }
     
     
