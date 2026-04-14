147ece50c7d19a8f712766dd9d71d245858ea142
xiaohwee82 <xiaohwee@yahoo.com>
2026-04-14 17:24:13 +0800

    refactor: update Employee and LeaveApplication with correct mappings

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/Employee.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/Employee.java
@@ -1,6 +1,9 @@
 package iss.nus.edu.sg.leave_application_processing_system.model;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
 import jakarta.persistence.*;
 
 @Entity
@@ -13,8 +16,23 @@ public class Employee {
   private String email;
   private String password;
   private String name;
-  private String role; // STAFF, MANAGER, ADMIN
+
   private String department;
+  
+  @Enumerated(EnumType.STRING)
+	private Role role; // STAFF, MANAGER, ADMIN
+  
+  @ManyToOne
+  @JoinColumn(name = "manager_id")
+  private Employee manager;
+  
+  @OneToMany(mappedBy = "employee")
+  @JsonIgnore // Prevents infinite loops in JSON serialization
+  private List<LeaveApplication> leaveApplications;
+  
+  @OneToMany(mappedBy = "manager") // currentManager.getSubordinates() to instantly get a list of their team members for leave approval.
+  @JsonIgnore
+  private List<Employee> subordinates;
   
   public Employee() {}
   
@@ -55,12 +73,20 @@ public class Employee {
     this.name = name;
   }
 
-  public String getRole() {
+  public Role getRole() {
     return role;
   }
 
-  public void setRole(String role) {
+  public void setRole(Role role) {
     this.role = role;
+  }
+
+  public Employee getManager() {
+	return manager;
+}
+
+  public void setManager(Employee manager) {
+	this.manager = manager;
   }
 
   public String getDepartment() {
@@ -70,7 +96,13 @@ public class Employee {
   public void setDepartment(String department) {
     this.department = department;
   }
-
+  public enum Role {
+		
+		STAFF,
+		MANAGER,
+		ADMIN
+		
+	}
   
 
   
--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/LeaveApplication.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/LeaveApplication.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveStatus;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,22 +27,48 @@ public class LeaveApplication {
 	@JoinColumn(name = "employee_id")
 	private Employee employee;
 	
+	@ManyToOne
+	@JoinColumn(name = "ledger_id")
+	private CompensationLedger ledger;
+	
+	@ManyToOne
+	@JoinColumn(name = "leaveEntitlement_id")
+	private LeaveEntitlement entitlement;
+	
+	@Enumerated(EnumType.STRING)
 	private LeaveType leaveType;
 	private LocalDate startDate;
 	private LocalDate endDate;
 	private LocalDateTime appliedAt;
+	
+	@Enumerated(EnumType.STRING)
 	private LeaveStatus status;
+	private String reason;
+	private String mgrRemarks;
+	private LocalDate updatedDate;
+	private boolean halfDay;
 	
 	public LeaveApplication() {}
-
-	public LeaveApplication(Employee employee, LeaveType leaveType, LocalDate startDate, LocalDate endDate) {
+	
+	
+	public LeaveApplication(Employee employee, CompensationLedger ledger, LeaveEntitlement entitlement,
+			LeaveType leaveType, LocalDate startDate, LocalDate endDate, LocalDateTime appliedAt, LeaveStatus status,
+			String reason, String mgrRemarks, LocalDate updatedDate, boolean halfDay) {
+	
 		this.employee = employee;
+		this.ledger = ledger;
+		this.entitlement = entitlement;
 		this.leaveType = leaveType;
 		this.startDate = startDate;
 		this.endDate = endDate;
-		this.appliedAt = LocalDateTime.now();
-		this.status = LeaveStatus.PENDING;
+		this.appliedAt = appliedAt;
+		this.status = status;
+		this.reason = reason;
+		this.mgrRemarks = mgrRemarks;
+		this.updatedDate = updatedDate;
+		this.halfDay = halfDay;
 	}
+
 	
 	//getters and setters
 	public Long getId() {
@@ -65,6 +93,23 @@ public class LeaveApplication {
 
 	public void setLeaveType(LeaveType leaveType) {
 		this.leaveType = leaveType;
+	}
+
+	
+	public CompensationLedger getLedger() {
+		return ledger;
+	}
+
+	public void setLedger(CompensationLedger ledger) {
+		this.ledger = ledger;
+	}
+
+	public LeaveEntitlement getEntitlement() {
+		return entitlement;
+	}
+
+	public void setEntitlement(LeaveEntitlement entitlement) {
+		this.entitlement = entitlement;
 	}
 
 	public LocalDate getStartDate() {
@@ -99,4 +144,68 @@ public class LeaveApplication {
 		this.status = status;
 	}
 		
+	public String getReason() {
+		return reason;
+	}
+
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+
+	public String getMgrRemarks() {
+		return mgrRemarks;
+	}
+
+
+	public void setMgrRemarks(String mgrRemarks) {
+		this.mgrRemarks = mgrRemarks;
+	}
+
+
+	public LocalDate getUpdatedDate() {
+		return updatedDate;
+	}
+
+
+	public void setUpdatedDate(LocalDate updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+
+	public boolean isHalfDay() {
+		return halfDay;
+	}
+
+
+	public void setHalfday(boolean halfDay) {
+		this.halfDay = halfDay;
+	}
+
+	public enum LeaveType {
+		
+		ANNUAL,
+		MEDICAL,
+		COMPENSATION
+		
+	}
+	
+	public enum LeaveStatus {
+		
+		APPLIED,
+		UPDATED,
+		APPROVED,
+		REJECTED,
+		DELETED,
+		CANCELLED,
+		ARCHIVED,
+		
+	}
+	
+	// Good to have for your business logic later
+	public long getDurationInDays() {
+	    if (startDate == null || endDate == null) return 0;
+	    return java.time.temporal.ChronoUnit.DAYS.between(startDate, endDate) + 1;
+	}
 }
