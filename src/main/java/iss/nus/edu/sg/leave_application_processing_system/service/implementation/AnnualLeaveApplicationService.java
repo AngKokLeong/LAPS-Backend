ba088b0e92be5385a4a24d4358a23e2dce623c77
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-15 11:27:44 +0800

    LAPSB-61 modified the specification rule name for better clarity

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/service/implementation/AnnualLeaveApplicationService.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/service/implementation/AnnualLeaveApplicationService.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Service;
 
 import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.ControllerDTO;
 import iss.nus.edu.sg.leave_application_processing_system.service.LeaveApplicationService;
-import iss.nus.edu.sg.leave_application_processing_system.service.DTO.AnnualLeaveServiceDTO;
+import iss.nus.edu.sg.leave_application_processing_system.service.DTO.LeaveApplicationServiceDTO;
 import iss.nus.edu.sg.leave_application_processing_system.service.DTO.ServiceDTO;
 
 
-import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.LeavePeriodFromIsWorkingDaySpecification;
-import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.LeavePeriodLessThanOrEqualFourteenDaysSpecification;
-import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.LeavePeriodToIsWorkingDaySpecification;
-import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.LeavePeriodWithinTheMonthSpecification;
+import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.AnnualLeavePeriodFromIsWorkingDaySpecification;
+import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.AnnualLeavePeriodLessThanOrEqualFourteenDaysSpecification;
+import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.AnnualLeavePeriodToIsWorkingDaySpecification;
+import iss.nus.edu.sg.leave_application_processing_system.service.business_logic.specification_impl.annual_leave_specification.AnnualLeavePeriodWithinTheMonthSpecification;
 
 @Service
 public class AnnualLeaveApplicationService implements LeaveApplicationService{
@@ -29,12 +29,12 @@ public class AnnualLeaveApplicationService implements LeaveApplicationService{
 
     @Override
     public Boolean submitApplication(ServiceDTO serviceDTO) {
-        AnnualLeaveServiceDTO dto = (AnnualLeaveServiceDTO) serviceDTO.getAllAttribute();
+        LeaveApplicationServiceDTO dto = (LeaveApplicationServiceDTO) serviceDTO.getAllAttribute();
 
-        boolean status = new LeavePeriodWithinTheMonthSpecification()
-                    .and(new LeavePeriodLessThanOrEqualFourteenDaysSpecification())
-                    .and(new LeavePeriodFromIsWorkingDaySpecification())
-                    .and(new LeavePeriodToIsWorkingDaySpecification())
+        boolean status = new AnnualLeavePeriodWithinTheMonthSpecification()
+                    .and(new AnnualLeavePeriodLessThanOrEqualFourteenDaysSpecification())
+                    .and(new AnnualLeavePeriodFromIsWorkingDaySpecification())
+                    .and(new AnnualLeavePeriodToIsWorkingDaySpecification())
                     .toPredicate()
                     .test(dto, dto);
 
