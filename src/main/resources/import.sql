880029d10a4bbc5a2eefd79299a7f2105f0e46fd
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-15 12:30:21 +0800

    LAPSB-71 added test value in import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO employees(department,email,name,password,role,manager_id,designation,joindate,status) VALUES ('ACCOUNTING','john@company.com','John','any','STAFF',NULL,NULL,NULL,NULL);
+
+INSERT INTO employees(department,email,name,password,role,manager_id,designation,joindate,status) VALUES ('ACCOUNTING','sarah@company.com','Sarah','any','MANAGER',NULL,NULL,NULL,NULL);
+
+INSERT INTO employees(department,email,name,password,role,manager_id,designation,joindate,status) VALUES ('ACCOUNTING','admin@company.com','Admin','password','ADMIN',NULL,NULL,NULL,NULL);
