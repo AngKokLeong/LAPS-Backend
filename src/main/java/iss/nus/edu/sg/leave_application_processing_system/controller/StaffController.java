8f8a4ac217346b5d335677b1be4ff55791ef649a
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-15 17:05:49 +0800

    LAPSB-72 added POSTMapping for annual leave application and setup getmapping for form submission

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/StaffController.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/StaffController.java
@@ -1,5 +1,6 @@
 package iss.nus.edu.sg.leave_application_processing_system.controller;
 
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,18 +9,33 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.ControllerDTO;
+import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.LeaveApplicationControllerDTO;
 import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.LeaveRequestControllerDTO;
 import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.constant.LeaveRequestStatusConstant;
 import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.constant.LeaveRequestTypeConstant;
 import iss.nus.edu.sg.leave_application_processing_system.controller.helper.LeaveRequestUtilities;
+import iss.nus.edu.sg.leave_application_processing_system.service.DTO.AnnualLeaveServiceDTO;
+import iss.nus.edu.sg.leave_application_processing_system.service.implementation.AnnualLeaveApplicationService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping ("/staff")
 public class StaffController {
 	
+	private final AnnualLeaveApplicationService annualLeaveApplicationService;
+
+	public StaffController(AnnualLeaveApplicationService annualLeaveApplicationService){
+		this.annualLeaveApplicationService = annualLeaveApplicationService;
+	}
+
+
+
 	@GetMapping ({"", "/"})
 	public String staffDashboard(HttpSession session) {
 		String role = (String) session.getAttribute("userRole");
@@ -29,14 +45,49 @@ public class StaffController {
 		return "dashboard";
 	}
 	
-	@GetMapping ("/apply-for-leave") 
-	public String applyForLeave(HttpSession session) {
+	@GetMapping ("/apply-leave") 
+	public String applyLeave(Model model, HttpSession session) {
 		String role = (String) session.getAttribute("userRole");
 		
 		if (role == null || role.toString().isEmpty()) return "redirect:/";
 		
-		return "apply-for-leave";
+		model.addAttribute("leaveApplication", new LeaveApplicationControllerDTO());
+
+		return "apply-leave";
 	}
+
+	@PostMapping("/apply-leave")
+	public String applyLeave(@ModelAttribute LeaveApplicationControllerDTO leaveApplication,Model model, HttpSession session){
+
+		//call the service class
+		AnnualLeaveServiceDTO annualLeaveServiceDTO = new AnnualLeaveServiceDTO();
+		annualLeaveServiceDTO.setStaffId(leaveApplication.getStaffId());
+		annualLeaveServiceDTO.setHalfDay(leaveApplication.isHalfDay());
+		annualLeaveServiceDTO.setLeavePeriodEnd(leaveApplication.getEndDate());
+		annualLeaveServiceDTO.setLeavePeriodStart(leaveApplication.getStartDate());
+		
+		annualLeaveServiceDTO.setReason(leaveApplication.getReason());
+		annualLeaveServiceDTO.setType(leaveApplication.getType());
+
+
+		ControllerDTO controllerDTO = annualLeaveApplicationService.submitApplication(annualLeaveServiceDTO);
+		LeaveApplicationControllerDTO leaveApplicationControllerDTO = (LeaveApplicationControllerDTO) controllerDTO.getAllAttribute();
+		//retrieve the result from the service class
+		if (leaveApplicationControllerDTO.getApplicationResult()){
+			//show success message
+			model.addAttribute("leaveApplicationInformation", leaveApplicationControllerDTO);
+		}else{
+			model.addAttribute("leaveApplicationInformation", leaveApplicationControllerDTO);
+		}
+
+
+
+		return "apply-leave";
+
+
+	}
+
+
 	
 	@GetMapping ("/my-leave-requests")
 	public String myLeaveRequests(Model model, HttpSession session) {
