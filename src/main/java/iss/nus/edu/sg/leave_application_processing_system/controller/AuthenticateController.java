b8dea8e74b0036a79e3c3e16a8c6d651822b30d7
jamiensim <jamienzxc@gmail.com>
2026-04-15 20:11:41 +0800

    Spring Security

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/AuthenticateController.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/AuthenticateController.java
@@ -1,68 +1,21 @@
 package iss.nus.edu.sg.leave_application_processing_system.controller;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.AuthenticationControllerDTO;
-import iss.nus.edu.sg.leave_application_processing_system.controller.DTO.ControllerDTO;
-import iss.nus.edu.sg.leave_application_processing_system.service.DTO.AuthenticationServiceDTO;
-import iss.nus.edu.sg.leave_application_processing_system.service.implementation.AuthenticationService;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/authenticate")
 public class AuthenticateController {
 
-	private final AuthenticationService authenticationService;
-
-	public AuthenticateController(AuthenticationService authenticationService){
-		this.authenticationService = authenticationService;
-	}
-
-
 	@GetMapping ("/")
 	public String showLandingPage() {
-		return "login";       
+		return "redirect:/";
 	}
 	
 	@GetMapping ("/login")
 	public String loginPage() {	
-		return "login";
-	}
-	
-	@GetMapping ("/logout")
-	public String logout(HttpSession session) {	
-		session.invalidate();
-		// Adding ?logout tells Thymeleaf to show the green box
-	    return "redirect:/authenticate/?logout"; 
-
-	}
-	
-	@PostMapping ("/login") 
-	public String validateLogin (@ModelAttribute AuthenticationControllerDTO authenticationUser, @RequestParam String email, @RequestParam String password, HttpSession session) {
-
-		AuthenticationServiceDTO authenticationServiceDTO = new AuthenticationServiceDTO();
-		authenticationServiceDTO.setEmail(authenticationUser.getEmail());
-		authenticationServiceDTO.setPassword(authenticationUser.getPassword());
-
-		Optional<ControllerDTO> controllerDTO = authenticationService.authenticateUser(authenticationServiceDTO);
-
-		if (controllerDTO.isPresent()){
-			AuthenticationControllerDTO authenticationControllerDTO = (AuthenticationControllerDTO)controllerDTO.get().getAllAttribute();
-			session.setAttribute("userRole", authenticationControllerDTO.getRole().toString());
-			session.setAttribute("id", authenticationControllerDTO.getStaffId());	
-
-			return "redirect:/staff";
-		}
-
-		return "failure";
+		return "redirect:/";
 	}
 	
 }
--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/service/EmployeeService.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/service/EmployeeService.java
@@ -3,6 +3,7 @@ package iss.nus.edu.sg.leave_application_processing_system.service;
 import iss.nus.edu.sg.leave_application_processing_system.repo.EmployeeRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import iss.nus.edu.sg.leave_application_processing_system.model.Employee;
 
@@ -10,12 +11,18 @@ import iss.nus.edu.sg.leave_application_processing_system.model.Employee;
 public class EmployeeService {
 	
 	private final EmployeeRepository employeeRepository;
+	private final PasswordEncoder passwordEncoder;
 	
-	public EmployeeService (EmployeeRepository employeeRepository) {
+	public EmployeeService (EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
 		this.employeeRepository = employeeRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 	
 	public boolean save(Employee employee) {
+		if (employee.getPassword() != null && !employee.getPassword().startsWith("$2")) {
+			employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+		}
+
 		Employee result = employeeRepository.save(employee);
 		if (result != null){
 			return true;
