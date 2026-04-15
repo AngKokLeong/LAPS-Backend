d4e9e7af4813e0f7220dc2bf3b4e428585075763
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-15 10:39:46 +0800

    LAPSB-68 added status and joindate attribute in Employee entity

--- src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/Employee.java
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/model/Employee.java
@@ -1,5 +1,7 @@
 package iss.nus.edu.sg.leave_application_processing_system.model;
 
+import java.time.LocalDate;
+
 import iss.nus.edu.sg.leave_application_processing_system.helper.Role;
 import jakarta.persistence.*;
 
@@ -27,9 +29,15 @@ public class Employee {
   @JoinColumn(name = "manager_id")
   private Employee manager;
 
+  private LocalDate joindate;
+  
+  private String status;
+
   // Constructors for testing
   public Employee() {}
   
+
+
   public Employee(String name, String email) {
     this.name = name;
     this.email = email;
@@ -92,5 +100,19 @@ public class Employee {
     this.manager = manager;
   }
 
+  public String getStatus() {
+    return status;
+  }
 
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public LocalDate getJoindate() {
+    return joindate;
+  }
+
+  public void setJoindate(LocalDate joindate) {
+    this.joindate = joindate;
+  }
 }
