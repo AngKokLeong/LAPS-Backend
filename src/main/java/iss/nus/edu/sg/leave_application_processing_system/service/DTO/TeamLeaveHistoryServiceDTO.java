e5636cf31157dfba3b730fd0853a5f8458dacb31
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-04-16 06:34:27 +0000

    LAPSB-86 added TeamLeaveHistory Service DTO

---
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/service/DTO/TeamLeaveHistoryServiceDTO.java
@@ -0,0 +1,110 @@
+package iss.nus.edu.sg.leave_application_processing_system.service.DTO;
+
+import java.time.LocalDate;
+
+import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveStatus;
+import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveType;
+
+public class TeamLeaveHistoryServiceDTO implements ServiceDTO {
+
+    private Long employeeId;
+    private String staffName;
+    private String staffDesignation;
+    private LeaveType leaveType;
+    private LocalDate leaveStartDate;
+    private LocalDate leaveEndDate;
+
+    private int numberOfLeaveDay;
+    private LeaveStatus leaveStatus;
+    private String reviewBy;
+    private LocalDate leaveRequestUpdatedDate;
+
+    public TeamLeaveHistoryServiceDTO() {
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public String getStaffDesignation() {
+        return staffDesignation;
+    }
+
+    public void setStaffDesignation(String staffDesignation) {
+        this.staffDesignation = staffDesignation;
+    }
+
+    public LeaveType getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(LeaveType leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public LocalDate getLeaveStartDate() {
+        return leaveStartDate;
+    }
+
+    public void setLeaveStartDate(LocalDate leaveStartDate) {
+        this.leaveStartDate = leaveStartDate;
+    }
+
+    public LocalDate getLeaveEndDate() {
+        return leaveEndDate;
+    }
+
+    public void setLeaveEndDate(LocalDate leaveEndDate) {
+        this.leaveEndDate = leaveEndDate;
+    }
+
+    public int getNumberOfLeaveDay() {
+        return numberOfLeaveDay;
+    }
+
+    public void setNumberOfLeaveDay(int numberOfLeaveDay) {
+        this.numberOfLeaveDay = numberOfLeaveDay;
+    }
+
+    public LeaveStatus getLeaveStatus() {
+        return leaveStatus;
+    }
+
+    public void setLeaveStatus(LeaveStatus leaveStatus) {
+        this.leaveStatus = leaveStatus;
+    }
+
+    public String getReviewBy() {
+        return reviewBy;
+    }
+
+    public void setReviewBy(String reviewBy) {
+        this.reviewBy = reviewBy;
+    }
+
+    public LocalDate getLeaveRequestUpdatedDate() {
+        return leaveRequestUpdatedDate;
+    }
+
+    public void setLeaveRequestUpdatedDate(LocalDate leaveRequestUpdatedDate) {
+        this.leaveRequestUpdatedDate = leaveRequestUpdatedDate;
+    }
+
+    @Override
+    public ServiceDTO getAllAttribute() {
+        return this;
+    }
+
+}
