591968de0397ae04859731a1eadea62f1b560f68
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-04-17 03:28:55 +0000

    LAPSB-96 added UpdateLeaveRequestServiceDTO for storing and processing data at Service layer

---
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/service/DTO/UpdateLeaveRequestServiceDTO.java
@@ -0,0 +1,85 @@
+package iss.nus.edu.sg.leave_application_processing_system.service.DTO;
+
+import java.time.LocalDate;
+
+import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveStatus;
+import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveType;
+
+public class UpdateLeaveRequestServiceDTO implements ServiceDTO {
+
+    private Long leaveRequestId;
+    private Long employeeId;
+
+    private LeaveType leaveType;
+    private LeaveStatus leaveStatus;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private String reason;
+
+    public UpdateLeaveRequestServiceDTO() {
+    }
+
+    public Long getLeaveRequestId() {
+        return leaveRequestId;
+    }
+
+    public void setLeaveRequestId(Long leaveRequestId) {
+        this.leaveRequestId = leaveRequestId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LeaveType getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(LeaveType leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public LeaveStatus getLeaveStatus() {
+        return leaveStatus;
+    }
+
+    public void setLeaveStatus(LeaveStatus leaveStatus) {
+        this.leaveStatus = leaveStatus;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    @Override
+    public ServiceDTO getAllAttribute() {
+        return this;
+    }
+
+}
