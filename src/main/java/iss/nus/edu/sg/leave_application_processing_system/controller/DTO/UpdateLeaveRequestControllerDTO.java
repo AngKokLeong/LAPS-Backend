9b81b3ba0bb73929ce8a0f42305705d6ce069576
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-04-17 03:28:21 +0000

    LAPSB-96 added UpdateLeaveRequestControllerDTO for storing and presenting data at Controller layer

---
+++ src/main/java/iss/nus/edu/sg/leave_application_processing_system/controller/DTO/UpdateLeaveRequestControllerDTO.java
@@ -0,0 +1,104 @@
+package iss.nus.edu.sg.leave_application_processing_system.controller.DTO;
+
+import java.time.LocalDate;
+
+import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveStatus;
+import iss.nus.edu.sg.leave_application_processing_system.helper.LeaveType;
+
+public class UpdateLeaveRequestControllerDTO implements ControllerDTO {
+
+    private Long leaveRequestId;
+    private Long employeeId;
+
+    private LeaveType leaveType;
+    private LeaveStatus leaveStatus;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private String reason;
+
+    private boolean operationResult;
+    private String operationComments;
+
+    public UpdateLeaveRequestControllerDTO() {
+    }
+
+    public Long getLeaveRequestId() {
+        return leaveRequestId;
+    }
+
+    public void setLeaveRequestId(Long leaveRequestId) {
+        this.leaveRequestId = leaveRequestId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LeaveType getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(LeaveType leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public LeaveStatus getLeaveStatus() {
+        return leaveStatus;
+    }
+
+    public void setLeaveStatus(LeaveStatus leaveStatus) {
+        this.leaveStatus = leaveStatus;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public boolean getOperationResult() {
+        return operationResult;
+    }
+
+    public void setOperationResult(boolean operationResult) {
+        this.operationResult = operationResult;
+    }
+
+    public String getOperationComments() {
+        return operationComments;
+    }
+
+    public void setOperationComments(String operationComments) {
+        this.operationComments = operationComments;
+    }
+
+    @Override
+    public ControllerDTO getAllAttribute() {
+        return this;
+    }
+
+}
